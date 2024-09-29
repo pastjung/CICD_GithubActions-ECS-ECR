7726a3e383a98fb5c2ea0725dc9dc4f7120552ae
정재현 <chwogus0303@naver.com>
2024-09-29 20:47:56 +0900

    feat(react-app): 코드 변경 사항 감지 추가
    vite.config.js & docker-compose.yml 파일의 volumes 사용

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -6,6 +6,9 @@ export default defineConfig({
   server: {
     host: true, // 외부에서 접근 가능하도록 설정
     port: process.env.VITE_REACT_SERVER_PORT, // 환경 변수에서 포트 읽기
+    watch: {
+      usePolling: true, // Polling 사용 : 주기적으로 polling 방식으로 파일 시스템을 확인하여 변경 사항을 감지 ( docker-compose.yml의 volumes와 함께 설정 )
+    },
   },
   plugins: [react()],
 })
