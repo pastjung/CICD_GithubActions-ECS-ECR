677f97b4d97c29c18b659dba0010a6979ef3219f
정재현 <chwogus0303@naver.com>
2024-09-29 21:20:30 +0900

    feat(react-app & springboot-app): 서비스 연결 및 CORS 설정

---
+++ springboot-app/src/main/java/com/inha/springbootapp/global/GlobalCorsConfig.java
@@ -0,0 +1,24 @@
+package com.inha.springbootapp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+@Configuration
+public class GlobalCorsConfig implements WebMvcConfigurer {
+
+    Dotenv dotenv = Dotenv.load();
+    private final String reactURL = "http://localhost:" + dotenv.get("REACT_HOST_PORT");
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해 CORS를 허용
+                .allowedOrigins(reactURL) // React 애플리케이션의 URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
+                .allowedHeaders("*") // 모든 헤더 허용
+                .allowCredentials(true); // 자격 증명(쿠키 등)을 허용할지 여부
+    }
+}
